61170de6d021d65c7acf013f3991220b9a101cc3
NgoMinhDuc28112001 <ngominhduc28112001@gmail.com>
2022-03-25 12:30:49 +0700

    NgoDuc 25/3 12:30AM

--- xac-nhan-hop-dong.php
+++ xac-nhan-hop-dong.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hợp đồng</title>
+    <title>Xác nhận hợp đồng</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
     <link rel="stylesheet" href="./CSS/base.css">
-    <link rel="stylesheet" href="./CSS/hopdong.css">
+    <link rel="stylesheet" href="./CSS/xac-nha-hop-dong.css">
     <link rel="stylesheet" href="./fontawesome-free-6.0.0-web/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
@@ -31,7 +31,10 @@
             <div class="content__title">
                 Yêu cầu thuê trọ đã được gửi đi, vui lòng chờ:
             </div>
-            <a href="./trangchu.php">Trở lại trang chủ</a>
+            <div class="content__a__i">
+                <i class="fa-solid fa-chevron-left"></i>
+                <a href="./trangchu.php">Trở lại trang chủ</a>
+            </div>
         </div>
         <!-- end content -->
         <footer class="footer">
